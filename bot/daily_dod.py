34dfbabc514ddfdaee71cb7c4177fffc9f7ac295
Andrew Curthoys <andrew-curthoys@system76-pc.localdomain>
2020-02-09 11:56:54 -0600

    Added functionality for tweets longer than 280 characters

--- bot/daily_dod.py
+++ bot/daily_dod.py
@@ -3,43 +3,75 @@ from keys import *
 import random
 import sqlite3
 from datetime import datetime
+import time
+
+# create status for tweeting or testing
+# test = 'tweet' will send a tweet
+# test = 'test' will print contents to terminal
+test = 'tweet'
 
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+def connect_to_twitter():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    return api
 
-# create a list of quote ids then a randomly shuffled list
-# of quote ids to "hash" the day of the year to a random id
-random.seed(7)
-quote_ids = list(range(1, 95))
-quote_id_hash = random.sample(quote_ids, len(quote_ids))
+def get_quote():
+    # create a list of quote ids then a randomly shuffled list
+    # of quote ids to "hash" the day of the year to a random id
+    random.seed(7)
+    quote_ids = list(range(1, 95))
+    quote_id_hash = random.sample(quote_ids, len(quote_ids))
 
-# create a dictionary of quote ids matched with their
-# randomly shuffled id "hash"
-id_dict = {}
-for id_, hash_ in zip(quote_ids, quote_id_hash):
-    id_dict[id_] = hash_
+    # create a dictionary of quote ids matched with their
+    # randomly shuffled id "hash"
+    id_dict = {}
+    for id_, hash_ in zip(quote_ids, quote_id_hash):
+        id_dict[id_] = hash_
 
-# get the day of the year, then take the mod of 95 and add 1
-# to get an id between 1 and 95 (the quote_id range in the DB)
-day_of_year = datetime.now().timetuple().tm_yday
-day_of_year_id = day_of_year % 95 + 1
-quote_id = id_dict[day_of_year_id]
+    # get the day of the year, then take the mod of 95 and add 1
+    # to get an id between 1 and 95 (the quote_id range in the DB)
+    day_of_year = datetime.now().timetuple().tm_yday
+    day_of_year_id = day_of_year % 95 + 1
+    quote_id = id_dict[day_of_year_id]
 
-# Connect to SQLite database
-conn = sqlite3.connect("../dick_or_don.db")
-db = conn.cursor()
+    # Connect to SQLite database
+    conn = sqlite3.connect("../dick_or_don.db")
+    db = conn.cursor()
 
-quote = db.execute('''SELECT
-                             quote
-                        FROM quotes
-                       WHERE quote_id = ?;''',
-                        (quote_id,))
-quote = quote.fetchone()
-quote = quote[0]
-conn.close()
+    quote = db.execute('''SELECT
+                                quote
+                            FROM quotes
+                        WHERE quote_id = ?;''',
+                            (quote_id,))
+    quote = quote.fetchone()
+    quote = quote[0]
+    conn.close()
 
-quote = f'Who the hell said this shit - Dick Nixon or Don Trump?\n\n"{quote}"'
+    quote = f'Who the hell said this shit - Dick Nixon or Don Trump?\n\n"{quote}"'
+    return quote
 
-api.update_status(quote)
+
+def send_tweet(api, quote):
+    api.update_status(quote)
+
+
+if __name__ == "__main__":
+    quote = get_quote()
+    if test == 'tweet':
+        api = connect_to_twitter()
+        if len(quote) <= 280:
+            send_tweet(api, quote)
+        else:
+            quote_1 = f'{quote[:277]}...'
+            quote_2 = quote[277:]
+            send_tweet(api, quote_1)
+            time.sleep(5)
+            tweet_1_id = api.user_timeline('dickordon')[0].id
+            api.update_status(quote_2, in_reply_to_status_id = tweet_1_id)
+    elif test == 'test':
+        print(len(quote))
+        print(quote)
+    else:
+        print('Must specify a test/tweet status!')
